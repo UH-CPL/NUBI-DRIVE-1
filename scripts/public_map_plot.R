a251eba2a49f806e8ab43230b3bd5958fc55efda
Huda Alghamdi <73255999+huda1412@users.noreply.github.com>
2023-02-07 10:40:41 -0600

    Update public_map_plot.R

--- scripts/public_map_plot.R
+++ scripts/public_map_plot.R
@@ -34,10 +34,10 @@ options(ggrepel.max.overlaps = Inf)
 
 #plot data
 plot <- ggplot(states, mapping = aes(long, lat, group = group))  + 
-  geom_polygon(fill = "light grey", colour = "dark grey") +
+  geom_polygon(fill = " grey", colour = "light grey") +
   coord_equal() +
   theme_void() +
-  geom_point(data=locations, aes( x = as.numeric(lon), y = as.numeric(lat),fill=PID),pch=21, size=3, stroke = 0.25,inherit.aes = FALSE,position=position_jitter(h=0,w=0.12), alpha = 0.75 )+
+  geom_point(data=locations, aes( x = as.numeric(lon), y = as.numeric(lat),fill=PID),pch=21, size=3, stroke = 0.25,inherit.aes = FALSE,position=position_jitter(h=0,w=0.1), alpha = 0.75 )+
   scale_fill_discrete(name=NULL)+
   geom_label_repel(data = p_unique_cities , aes( x = as.numeric(locations.lon)+0.06, y = as.numeric(locations.lat)+0.06, label=paste0(locations.city," / ",locations.city_count)),size = 5,direction="both", max.overlaps = Inf,colour="black", force=1, box.padding = 1,inherit.aes = FALSE )+
   theme(plot.margin=grid::unit(c(10,10,10,10), "mm"))
@@ -49,4 +49,5 @@ ggsave(
   height = 8.5,
   plot
 )
+
 
